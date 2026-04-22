699d1fcfa185188ba3633f5078e2551990deafba
unknown <ripon.allreg@gmail.com>
2026-04-22 16:49:20 +0600

    fix: default tab to Metrics on /prices page

--- components/features/prices/PricesPageClient.tsx
+++ components/features/prices/PricesPageClient.tsx
@@ -5,7 +5,7 @@ import MetricsView from "./MetricsView";
 import PricesView from "./PricesView";
 
 export default function PricesPageClient() {
-  const [tab, setTab] = useState<"metrics" | "prices">("prices");
+  const [tab, setTab] = useState<"metrics" | "prices">("metrics");
 
   return (
     <div className="relative z-[2] max-w-[1440px] mx-auto px-3 md:px-10 pb-10 pt-4">
